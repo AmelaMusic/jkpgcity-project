52d25fa32886e8287830e4b6b09d68fb02c3c3b2
AmelaMusic <muam21gk@student.ju.se>
2025-02-27 17:23:20 +0100

    Add index,css app files

---
+++ public/app.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const venuesList = document.getElementById("venues-list");
+  venuesList.innerHTML = "<p>Venues will be displayed here.</p>";
+});
